3c69fbff1df2462769e1753b61ffd3c2bd772de6
klink18 <kate.g.link@gmail.com>
2016-03-14 11:56:21 -0600

    Update testMultigrid1D.m

--- testMultigrid1D.m
+++ testMultigrid1D.m
@@ -5,6 +5,7 @@
 %                   'vcylce.m'
 %                   'wJacobi.m'
 % are working properly
+
 close all, clear all
 
 %Set-Up of Spatial Step and 1-D Domain = [0,1]
@@ -12,14 +13,14 @@ h=(1/(2^(5)));
 x=[0:h:1];
 xInner=x(2:end-1);
 
-% %Test - 1
-% %A quandratic with homogeneous Dirichlet boundary conditions
+%Test - 1
+%A quandratic with homogeneous Dirichlet boundary conditions
 % 
 %True Solution
 phi = @(x) -(x-.5).^2 + .25*ones(size(x));
 utrue1=phi(xInner);
 
-%Guess
+%Guesses
 guess1 = sin(2*pi*9*xInner);
 %guess1 = zeros(size(xInner));
 %guess1 = rand(size(xInner));
@@ -39,7 +40,6 @@ T=legend('Initial Guess');
 xlim([0,1])
 xlabel('$x$','interpreter','latex','FontSize',18)
 set(T,'Location','Best')
-%ylim([0, 3])
 title('Test 2', 'interpreter','latex');
 set(gca,'Fontsize',14)
 hold off
@@ -53,7 +53,6 @@ T=legend('Numerical Solution','True Solution');
 xlim([0,1])
 xlabel('$x$','interpreter','latex','FontSize',18)
 set(T,'Location','Best')
-%ylim([0, 3])
 title('Test 2 - Results', 'interpreter','latex');
 set(gca,'Fontsize',14)
 hold off
@@ -61,60 +60,57 @@ string2=strcat('figTest2Results',num2str(h),num2str(it1));
 name2=strcat('\Users\klink\Documents\GradSchool\SPRING2016\NumPDE\',string2,'.eps');
 saveas(gcf,name2,'epsc');
 
-% %Test - 2
-% %Zero right-hand side and homogeneous Dirichlet boundary conditions
+%Test - 2
+%Zero right-hand side and homogeneous Dirichlet boundary conditions
 % 
-% %Set-Up of Spatial Step and 1-D Domain = [0,1]
-% % h=(1/32);
+%Set-Up of Spatial Step and 1-D Domain = [0,1]
+% %h=(1/32);
 % % x=[0:h:1];
 % % xInner=x(2:end-1);
 % 
 % %True Solution = zero
-% utrue=zeros(size(xInner));
+utrue=zeros(size(xInner));
 % 
-% %Guess
-% 
+% %Guesses
 % %guess2 = ()(size(xInner));
 % %guess2 = (1e-10)*ones(size(xInner));
-% guess2 = sin(2*pi*9*xInner);
+guess2 = sin(2*pi*9*xInner);
 % %guess2 = rand(size(xInner));
 % 
 % %Right Hand Side
-% f2=zeros(size(guess2));
+f2=zeros(size(guess2));
 % 
 % %Call Multigrid
-% [u2,it2] = multigrid(guess2,f2,h,utrue);
+[u2,it2] = multigrid(guess2,f2,h,utrue);
 % 
-% uFinal2=[0 u2' 0];
+uFinal2=[0 u2' 0];
 % 
-% %Plot Results
+%Plot Results
 % 
-% figure(4)
-% plot(x,[0 guess2 0],'r','LineWidth',2)
-% T=legend('Initial Guess');
-% xlim([0,1])
-% xlabel('$x$','interpreter','latex','FontSize',18)
-% set(T,'Location','Best')
-% %ylim([0, 3])
-% title('Test 1', 'interpreter','latex');
-% set(gca,'Fontsize',14)
-% hold off
-% string4=strcat('figTest1InitialGuess',num2str(h),num2str(it2));
-% name4=strcat('\Users\klink\Documents\GradSchool\SPRING2016\NumPDE\',string4,'.eps');
-% saveas(gcf,name4,'epsc');
-% 
-% figure(5)
-% plot(x,uFinal2,'-xb',x,zeros(size(x)),'--r','Linewidth',2)
-% T=legend('Numerical Solution','True Solution');
-% xlim([0,1])
-% xlabel('$x$','interpreter','latex','FontSize',18)
-% set(T,'Location','Best')
-% %ylim([0, 3])
-% title('Test 1- Results', 'interpreter','latex');
-% set(gca,'Fontsize',14)
-% hold off
-% string5=strcat('figTest1Results',num2str(h),num2str(it2));
-% name5=strcat('\Users\klink\Documents\GradSchool\SPRING2016\NumPDE\',string5,'.eps');
-% saveas(gcf,name5,'epsc');
+figure(4)
+plot(x,[0 guess2 0],'r','LineWidth',2)
+T=legend('Initial Guess');
+xlim([0,1])
+xlabel('$x$','interpreter','latex','FontSize',18)
+set(T,'Location','Best')
+title('Test 1', 'interpreter','latex');
+set(gca,'Fontsize',14)
+hold off
+string4=strcat('figTest1InitialGuess',num2str(h),num2str(it2));
+name4=strcat('\Users\klink\Documents\GradSchool\SPRING2016\NumPDE\',string4,'.eps');
+saveas(gcf,name4,'epsc');
+ 
+figure(5)
+plot(x,uFinal2,'-xb',x,zeros(size(x)),'--r','Linewidth',2)
+T=legend('Numerical Solution','True Solution');
+xlim([0,1])
+xlabel('$x$','interpreter','latex','FontSize',18)
+set(T,'Location','Best')
+title('Test 1- Results', 'interpreter','latex');
+set(gca,'Fontsize',14)
+hold off
+string5=strcat('figTest1Results',num2str(h),num2str(it2));
+name5=strcat('\Users\klink\Documents\GradSchool\SPRING2016\NumPDE\',string5,'.eps');
+saveas(gcf,name5,'epsc');
 
-ITERS=[it1]';
+ITERS=[it1 it2]';
